edad9204590768a889d25722438dfaf3ee4a80e5
LucasDuval <lucas.duval@sigmalis.ch>
2022-01-11 16:47:53 +0100

    Initial commit

---
+++ MyString.cpp
@@ -0,0 +1,17 @@
+#include "MyString.h"
+
+MyString::MyString(){
+
+}
+
+MyString::MyString(float a, float b){
+
+}
+
+MyString::~MyString(){
+	
+}
+
+MyString::MyString(const MyString& string_to_copy){
+
+}
